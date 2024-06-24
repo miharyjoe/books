446e6418b7686ff2ec55766883ef39da00d0443c
miharyjoel <miharyjoel@gmail.com>
2024-06-24 19:49:38 +0300

    feat: init project

---
+++ book/src/test/java/com/miharyjoel/book/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miharyjoel.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
