cbc91944971461bf532750b6a95cb92d99d86c28
miharyjoel <miharyjoel@gmail.com>
2024-07-02 22:00:47 +0300

    feat: add user model

--- book/src/main/java/com/miharyjoel/book/BookApiApplication.java
+++ book/src/main/java/com/miharyjoel/book/BookApiApplication.java
@@ -2,8 +2,10 @@ package com.miharyjoel.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookApiApplication {
 
 	public static void main(String[] args) {
