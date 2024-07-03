676a876723c2b77bd3d41d28ac7ac6b913f666b7
miharyjoel <miharyjoel@gmail.com>
2024-07-03 16:07:49 +0300

    feat: add user model

--- book/src/main/java/com/miharyjoel/book/model/User.java
+++ book/src/main/java/com/miharyjoel/book/model/User.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -26,7 +28,7 @@ import java.util.List;
 public class User implements UserDetails, Principal {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue
   private Long id;
 
   private String firstanme;
@@ -37,6 +39,9 @@ public class User implements UserDetails, Principal {
   private String password;
   private boolean accountLocked;
   private boolean enabled;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  private List<Role> roles;
 
   @CreatedDate
   @Column(nullable = false, updatable = false)
@@ -53,7 +58,10 @@ public class User implements UserDetails, Principal {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
+    return this.roles
+      .stream()
+      .map(r -> new SimpleGrantedAuthority(r.getName()))
+      .collect(Collectors.toList());
   }
 
   @Override
