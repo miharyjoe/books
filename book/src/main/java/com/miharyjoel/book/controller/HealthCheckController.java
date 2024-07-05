51421cc723d6928fb9b0bd88467025fbec660011
miharyjoel <miharyjoel@gmail.com>
2024-07-05 22:24:26 +0300

    feat: add healthcheck api

---
+++ book/src/main/java/com/miharyjoel/book/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.miharyjoel.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+  @GetMapping("/ping")
+  public String ping(){
+    return "pong";
+  }
+}
