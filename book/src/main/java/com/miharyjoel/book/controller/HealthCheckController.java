63d4c2db91c4a69025ce3b104d4f5fe3e0fff234
miharyjoel <miharyjoel@gmail.com>
2024-07-10 20:21:50 +0300

    fix: health controller

--- book/src/main/java/com/miharyjoel/book/controller/HealthCheckController.java
+++ book/src/main/java/com/miharyjoel/book/controller/HealthCheckController.java
@@ -1,9 +1,11 @@
 package com.miharyjoel.book.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("health")
 public class HealthCheckController {
 
   @GetMapping("/ping")
--- book/src/main/java/com/miharyjoel/book/security/SecurityConfig.java
+++ book/src/main/java/com/miharyjoel/book/security/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.config.http.SessionCreationPolicy.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -24,31 +24,33 @@ public class SecurityConfig {
   private final AuthenticationProvider authenticationProvider;
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
       .cors(withDefaults())
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(req ->
         req.requestMatchers(
-          "/auth/**",
-          "/v2/api-docs",
-          "/v3/api-docs",
-          "/v3/api-docs/**",
-          "/swagger-resources",
-          "/swagger-resources/**",
-          "/configuration/ui",
-          "/configuration/security",
-          "/swagger-ui/**",
-          "/webjars/**",
-          "/swagger-ui.html"
-        ).permitAll()
+          "/health/ping",
+            "/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+          )
+          .permitAll()
           .anyRequest()
           .authenticated()
       )
       .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
       .authenticationProvider(authenticationProvider)
       .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
     return http.build();
   }
-
 }
