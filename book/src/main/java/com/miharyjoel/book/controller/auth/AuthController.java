a015f9cabdd6a4e1850ae5ac7304c60fa9af1062
miharyjoel <miharyjoel@gmail.com>
2024-07-14 19:15:54 +0300

    feat: add authenticate controller

--- book/src/main/java/com/miharyjoel/book/controller/auth/AuthController.java
+++ book/src/main/java/com/miharyjoel/book/controller/auth/AuthController.java
@@ -1,5 +1,7 @@
 package com.miharyjoel.book.controller.auth;
 
+import com.miharyjoel.book.dto.AuthenticationRequest;
+import com.miharyjoel.book.dto.AuthenticationResponse;
 import com.miharyjoel.book.dto.RegistrationRequest;
 import com.miharyjoel.book.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,4 +28,13 @@ public class AuthController {
     service.register(request);
     return ResponseEntity.accepted().build();
   }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+     @Valid @RequestBody AuthenticationRequest request
+  ){
+    return ResponseEntity.ok(service.authenticate(request));
+  };
+
+
 }
