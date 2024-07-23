46b4ac38ea50869bcadbb987e7f7e49423977538
miharyjoel <miharyjoel@gmail.com>
2024-07-23 22:30:59 +0300

    feat: add feedback entity

--- book/src/main/java/com/miharyjoel/book/handler/GlobalExceptionHandler.java
+++ book/src/main/java/com/miharyjoel/book/handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.miharyjoel.book.handler;
 
 import jakarta.mail.MessagingException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
---
+++ book/src/main/java/com/miharyjoel/book/model/Feedback.java
@@ -0,0 +1,26 @@
+package com.miharyjoel.book.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feedback extends BaseEntity{
+
+  private Double note;
+
+  private String comment;
+
+  @ManyToOne
+  @JoinColumn(name = "book_id")
+  private Book book;
+}
