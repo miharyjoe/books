7e3b9d8db12e95c7e25b0cf9452c64d098260c26
miharyjoel <miharyjoel@gmail.com>
2024-07-28 21:52:58 +0300

    feat: add query findAllBook

--- book/src/main/java/com/miharyjoel/book/repository/BookRepository.java
+++ book/src/main/java/com/miharyjoel/book/repository/BookRepository.java
@@ -1,9 +1,22 @@
 package com.miharyjoel.book.repository;
 
 import com.miharyjoel.book.model.Book;
+import com.miharyjoel.book.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+  @Query("""
+    SELECT book
+    FROM Book book
+    WHERE book.archived = false
+    AND book.shareable = true
+    AND book.owner.id != :userId
+    """)
+  Page<Book> findAllDisplayableBoos(Pageable pageable, Long userId);
 }
