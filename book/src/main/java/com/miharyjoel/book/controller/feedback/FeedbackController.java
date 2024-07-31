39d35891d641bf4f9608ccf36d685c304007e634
miharyjoel <miharyjoel@gmail.com>
2024-07-31 21:39:28 +0300

    feat:get  feedback by book id

--- book/src/main/java/com/miharyjoel/book/controller/feedback/FeedbackController.java
+++ book/src/main/java/com/miharyjoel/book/controller/feedback/FeedbackController.java
@@ -1,16 +1,15 @@
 package com.miharyjoel.book.controller.feedback;
 
 import com.miharyjoel.book.dto.FeedbackRequest;
+import com.miharyjoel.book.dto.FeedbackResponse;
+import com.miharyjoel.book.dto.PageResponse;
 import com.miharyjoel.book.service.FeedbackService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("feedbacks")
@@ -20,11 +19,21 @@ public class FeedbackController {
 
     private final FeedbackService service;
 
-    @PostMapping
+  @PostMapping
   public ResponseEntity<Long> saveFeedback(
     @Valid @RequestBody FeedbackRequest request,
     Authentication connectedUser
     ){
     return ResponseEntity.ok(service.save(request, connectedUser));
     }
+
+  @GetMapping("/book/{book_id}")
+  public ResponseEntity<PageResponse<FeedbackResponse>> findAllFeedbackByBook(
+    @PathVariable("book_id") Long bookId,
+    @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+    @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+    Authentication connectedUser
+  ){
+  return ResponseEntity.ok(service.findAllFeedbackByBook(bookId, page,size,connectedUser));
+  }
 }
